6efeace0c7e268504d3f21228de140659e338360
syed bayzid <husains82849@gmail.com>
2026-06-02 11:49:30 +0600

    Solve Simple Calculator problem with C++

---
+++ C++/practice_day1_m2.5/C_Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << X <<" + "<< Y <<" = "<< X+Y <<endl;
+    cout << X <<" * "<< Y <<" = "<< X*Y <<endl;
+    cout << X <<" - "<< Y <<" = "<< X-Y <<endl;
+
+
+}
