1d9cc7976349b8724aef72f7b2db20047817e1d1
syed bayzid <husains82849@gmail.com>
2026-06-04 09:46:17 +0600

    Solve mathematical expression problem with C++

---
+++ C++/practice_day_2_m3.5/W_Mathematical_Expression.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin >> A;
+    char S;
+    cin >> S;
+    int B;
+    cin >> B;
+    char Q;
+    cin >> Q;
+    int C;
+    cin >> C;
+
+    if (S == '+')
+    {
+        if (A + B == C)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << A + B << endl;
+        }
+    }
+
+    if (S == '-')
+    {
+        if (A - B == C )
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << A - B << endl;
+        }
+    }
+
+    if (S == '*')
+    {
+        if (A * B == C)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << A * B << endl;
+        }
+    }
+}
