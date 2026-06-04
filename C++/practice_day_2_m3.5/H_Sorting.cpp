9c351433ae1a8ce08033e898015c7da4da5efb40
syed bayzid <husains82849@gmail.com>
2026-06-04 17:09:52 +0600

    Solve sorting problem using selection sort with C++

---
+++ C++/practice_day_2_m3.5/H_Sorting.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int A[N];
+
+    
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i <= N - 2; i++)
+    {
+        for (int j = 0; j <= N - 2 - i; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+}
