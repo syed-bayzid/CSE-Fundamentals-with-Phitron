bca6aef918fae70f169522278d3ab1b7d062cc45
syed bayzid <husains82849@gmail.com>
2026-06-09 12:03:39 +0600

    Solved CF Way Too Long Words using string logic with C++

---
+++ C++/string_class_m5/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int len = s.size();
+
+    char first, last;
+
+    first = s[0];
+    last = s[len - 1];
+
+    if(len > 10) {
+        cout << first << len - 2 << last << endl;
+    }
+    else {
+        cout << s << endl;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ C++/string_class_m5/string.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str = "Hello, World!";
+    string str2 = "Hello, World!";
+
+    str.clear(); // Clears the content of the string, making it empty
+    cout << "str: " << str << endl; // Output will be empty
+    cout << "str2: " << str2 << endl; // Output will be "
+}
