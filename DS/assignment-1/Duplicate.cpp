7ba39748912f8f17f6347c096feecc08b8e0e08e
syed bayzid <husains82849@gmail.com>
2026-06-26 17:12:36 +0600

    Complete HackerRank Duplicate problem solution

---
+++ DS/assignment-1/Duplicate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (A[i] == A[j])
+            {
+                cout << "YES";
+                return 0;
+            }
+        }
+    }
+
+    cout << "NO";
+
+    return 0;
+}
